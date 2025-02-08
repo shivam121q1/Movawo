a2fbff903f654343f37500134d73f8c2f15deb09
shivam121q1 <shivampal7006@gmail.com>
2025-02-08 17:06:12 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.scss'
+import App from './App.jsx'
 import { store } from './store/store.js'
 import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store} >
-      <App />
-  </Provider>
-    
- 
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 )
+
